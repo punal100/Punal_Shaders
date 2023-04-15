eef7cbf664c2bb915dea970d67900f596b889cc0
Punal Manalan <punal100@gmail.com>
2023-04-16 00:57:45 +0530

    Update By Punal Manalan, Updates: 1. Plugin Updated.
    Signed-off-by: Punal Manalan <punal100@gmail.com>

--- Source/Punal_Shaders/Private/Punal_Shaders.cpp
+++ Source/Punal_Shaders/Private/Punal_Shaders.cpp
@@ -7,6 +7,11 @@
 void FPunal_ShadersModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	FString ShaderDir = FPaths::Combine(FPaths::ProjectPluginsDir(), TEXT("Punal_Shaders/Shaders"));
+	AddShaderSourceDirectoryMapping("/Plugin/Punal_Shaders", ShaderDir);
+
+	FString ProjectShaderDir = FPaths::Combine(FPaths::ProjectDir(), TEXT("/Shaders"));
+	AddShaderSourceDirectoryMapping("/Project/Shaders", ProjectShaderDir);
 }
 
 void FPunal_ShadersModule::ShutdownModule()
@@ -14,7 +19,7 @@ void FPunal_ShadersModule::ShutdownModule()
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
 }
-
+  
 #undef LOCTEXT_NAMESPACE
 	
 IMPLEMENT_MODULE(FPunal_ShadersModule, Punal_Shaders)
--- Source/Punal_Shaders/Punal_Shaders.Build.cs
+++ Source/Punal_Shaders/Punal_Shaders.Build.cs
@@ -38,6 +38,7 @@ public class Punal_Shaders : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
+				"RenderCore",
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
